66dc7e94c83084762e814b7ccb4833304ee54583
Francisco Santos <fwfs1985@gmail.com>
2020-05-20 18:08:38 -0300

    make footer

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,18 +3,21 @@ import PropTypes from 'prop-types'
 import siteConfig from '../../../data/siteConfig'
 import { ResetCSS, GlobalStyle } from '../styles'
 import Header from '../header'
+import Footer from '../footer'
 
 const Layout = ({ children }) => (
   <React.Fragment>
-    <Header headerLinks={siteConfig.headerLinks} />
     <ResetCSS />
     <GlobalStyle />
+    <Header headerLinks={siteConfig.headerLinks} />
     <div>{children}</div>
+    <Footer />
   </React.Fragment>
 )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
+
 
 export default Layout
