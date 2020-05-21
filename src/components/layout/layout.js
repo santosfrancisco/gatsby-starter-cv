47c117b21a5b55d6b1292a32d5b3f3f64a46ae1b
Francisco Santos <fwfs1985@gmail.com>
2020-05-20 22:05:01 -0300

    fix header

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import siteConfig from '../../../data/siteConfig'
 import { ResetCSS, GlobalStyle } from '../styles'
 import Header from '../header'
 import Footer from '../footer'
 
-const Layout = ({ children }) => (
+const Layout = ({ children, location }) => (
   <React.Fragment>
     <ResetCSS />
     <GlobalStyle />
-    <Header headerLinks={siteConfig.headerLinks} />
+    <Header location={location} />
     <div>{children}</div>
     <Footer />
   </React.Fragment>
