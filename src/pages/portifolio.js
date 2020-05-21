96ab9bc898edca9b05b0557b1afdf98bb3b08b99
Francisco Santos <fwfs1985@gmail.com>
2020-05-20 22:22:44 -0300

    create portifolio page

---
+++ src/pages/portifolio.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { Container, Row, Col, config } from 'react-awesome-styled-grid'
+import siteConfig from '../../data/siteConfig'
+
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import SEO from '../components/SEO'
+import Wrapper from '../components/wrapper'
+
+const Image = styled.img`
+  max-height: 220px;
+  max-width: 220px;
+  object-fit: cover;
+  object-position: center center;
+  border-radius: 10px;
+  box-shadow: 24px 47px 79px -21px rgba(0,0,0,0.51);
+`
+
+const JobCard = styled.a`
+  text-decoration: none;
+  color: inherit;
+
+  ${({ href }) => href && css`
+    &:hover ${Image}{
+      transition: transform .5s;
+      transform: translateY(-5px);
+    }
+  `}
+`
+
+const Portifolio = ({ className, location }) => {
+  const title = "Portifolio"
+  const { keywords, portifolio } = siteConfig
+  return (
+    <Layout location={location}>
+      <SEO
+        title={title}
+        keywords={keywords}
+      />
+
+      <Hero
+        heroImg="/images/pierre-chatel-innocenti-W5INoOK-5eI-unsplash.jpg"
+        title={title}
+      />
+
+      <Wrapper className={className}>
+        <Container className="page-content" fluid>
+          <Row>
+            {portifolio.map(job => (
+              <Col
+                key={job.description}
+                align="center"
+              >
+                <JobCard
+                  as={job.url ? "a" : "div"}
+                  href={job.url}
+                  target="_blank"
+                >
+                  <Image src={job.image} />
+                  <p>{job.description}</p>
+                </JobCard>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export default styled(Portifolio)`
+  .page-content {
+    max-width: 100%;
+    margin-bottom: 40px;
+  }
+
+`
