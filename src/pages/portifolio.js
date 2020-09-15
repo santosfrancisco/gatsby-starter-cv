7b92094fd6dfee7e8f4b0d57b4b4ed7528f76e2c
gallinaro <53143045+gallinaro@users.noreply.github.com>
2020-09-15 18:46:39 +0200

    Update portifolio.js

--- src/pages/portifolio.js
+++ src/pages/portifolio.js
@@ -32,7 +32,7 @@ const JobCard = styled.a`
 `
 
 const Portifolio = ({ className, location }) => {
-  const title = "Portifolio"
+  const title = "Progetti ed interessi"
   const { keywords, portifolio } = siteConfig
   return (
     <Layout location={location}>
