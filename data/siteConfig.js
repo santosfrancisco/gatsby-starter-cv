8558b8dd245c9cc63bc4a6c3db7c98c5ac914956
gallinaro <53143045+gallinaro@users.noreply.github.com>
2020-09-15 15:22:34 +0200

    Update siteConfig.js

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -6,10 +6,8 @@ module.exports = {
   twitterUsername: '',
   githubUsername: '',
   authorAvatar: '/images/foto.cv.jpg',
-  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
-    In 2012 I started working as a support technician and I approached the developers.
-    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
-    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  authorDescription: `Ex giocatore di pallavolo appassionato da sempre di tecnologia.
+Ho acquisito una buona capacità comunicativa, organizzativa e di problem solving durante i periodi lavorativi e durante il periodo in cui ho assunto il ruolo di aiuto allenatore in 2 squadre di pallavolo.`,
   skills: [
     {
       name: 'HTML',
