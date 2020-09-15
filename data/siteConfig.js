afc654615835396781eefbf5cad4eed68f0b30b6
gallinaro <53143045+gallinaro@users.noreply.github.com>
2020-09-15 14:54:44 +0200

    Update siteConfig.js

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
+  siteTitle: 'Gallinaro Marco',
   siteDescription: `Create your online curriculum in just a few minutes with this starter`,
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
+  authorName: 'Gallinaro Marco',
+  twitterUsername: '',
+  githubUsername: '',
   authorAvatar: '/images/avatar.jpeg',
   authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
     In 2012 I started working as a support technician and I approached the developers.
