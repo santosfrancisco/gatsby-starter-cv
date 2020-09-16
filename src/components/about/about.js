6ab9152bf2be8a7e7eb77e1b80535d9b5387cbd6
gallinaro <53143045+gallinaro@users.noreply.github.com>
2020-09-16 10:19:19 +0200

    Update about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,7 +5,7 @@ export default ({ title = 'about', text = '' }) => {
     <div>
       <h1>{title}</h1>
       <p dangerouslySetInnerHTML={{ __html: text }}></p>
-    <a href="gatsby-starter-cv/static/images/cv aggiornato 2020.pdf" download> <img src="gatsby-starter-cv/static/images/download_img.png" width="104" height="142"></a>
+    <a href="gatsby-starter-cv/static/images/cv aggiornato 2020.pdf" download> <img src="https://raw.githubusercontent.com/gallinaro/gatsby-starter-cv/master/static/images/download_img.png" width="104" height="142"></a>
     </div>
     
   )
